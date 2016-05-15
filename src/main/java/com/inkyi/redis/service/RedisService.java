5d9112af9ecbe784ffd371b931fe88dedbba9483
InkYi <lhkmaple@gmail.com>
2016-05-16 02:50:05 +0800

    更新redis的Service

--- src/main/java/com/inkyi/redis/service/RedisService.java
+++ src/main/java/com/inkyi/redis/service/RedisService.java
@@ -4,33 +4,7 @@ import java.util.Set;
 
 public interface RedisService {
 	/**
-	 * 通过key删除
-	 * 
-	 * @param key
-	 */
-	public abstract long del(String... keys);
-
-	/**
-	 * 添加key value 并且设置存活时间(byte)
-	 * 
-	 * @param key
-	 * @param value
-	 * @param liveTime
-	 */
-	public abstract void set(byte[] key, byte[] value, long liveTime);
-
-	/**
-	 * 添加key value 并且设置存活时间
-	 * 
-	 * @param key
-	 * @param value
-	 * @param liveTime
-	 *            单位秒
-	 */
-	public abstract void set(String key, String value, long liveTime);
-
-	/**
-	 * 添加key value
+	 * 将字符串值 value 关联到 key 。
 	 * 
 	 * @param key
 	 * @param value
@@ -38,7 +12,7 @@ public interface RedisService {
 	public abstract void set(String key, String value);
 
 	/**
-	 * 添加key value (字节)(序列化)
+	 * 将字符串值 value 关联到 key 。
 	 * 
 	 * @param key
 	 * @param value
@@ -46,46 +20,195 @@ public interface RedisService {
 	public abstract void set(byte[] key, byte[] value);
 
 	/**
-	 * 获取redis value (String)
+	 * 删除
 	 * 
-	 * @param key
+	 * @param keys
 	 * @return
+	 */
+	public abstract Long del(String... keys);
+
+	/**
+	 * 删除
+	 * 
+	 * @param keys
+	 * @return
+	 */
+	public abstract Long del(byte[]... keys);
+
+	/**
+	 * 获取key
 	 */
 	public abstract String get(String key);
 
 	/**
-	 * 通过正则匹配keys
-	 * 
-	 * @param pattern
-	 * @return
+	 * 获取key
 	 */
-	public abstract Set<String> Setkeys(String pattern);
+	public abstract byte[] get(byte[] key);
 
 	/**
-	 * 检查key是否已经存在
+	 * 执行脚本
+	 * 
+	 * @param script
+	 *            脚本
+	 * @param resultType
+	 *            返回值类型
+	 * @param value
+	 *            脚本内参数(依照数据顺序KEYS[1],KEYS[2])
+	 * @return
+	 */
+	public abstract <T> T eval(String script, Class<T> resultType,
+			String... value);
+
+	/**
+	 * 执行脚本
+	 * 
+	 * @param scriptSha
+	 * @param resultType
+	 * @param value
+	 * @return
+	 */
+	public abstract <T> T evalSha(String scriptSha, Class<T> resultType,
+			String... value);
+
+	/**
+	 * list.add
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public abstract Long lPush(String key, String value);
+
+	/**
+	 * list.addall
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public abstract Long lPush(String key, String... value);
+
+	/**
+	 * list.size
 	 * 
 	 * @param key
 	 * @return
 	 */
-	public abstract boolean exists(String key);
+	public abstract Long lLen(String key);
 
 	/**
-	 * 清空redis 所有数据
+	 * 删除目标redis全部DB缓存
+	 */
+	public abstract void flushAll();
+
+	/**
+	 * 预加载脚本
 	 * 
+	 * @param script
+	 * @return sha值
+	 */
+	public abstract String scriptLoad(String script);
+
+	public abstract Long lPush(String key, String value, Long seconds);
+
+	/**
+	 * key 添加有效时间
+	 * 
+	 * @param key
+	 * @param seconds
 	 * @return
 	 */
-	public abstract String flushDB();
+	public abstract Boolean expire(String key, Long seconds);
 
 	/**
-	 * 查看redis里有多少数据
-	 */
-	public abstract long dbSize();
-
-	/**
-	 * 检查是否连接成功
+	 * 判断map中是否存在
 	 * 
+	 * @param key
+	 *            redis 中的 key
+	 * @param hKey
+	 *            Map 中的key
 	 * @return
 	 */
-	public abstract String ping();
+	public abstract Boolean hExists(String key, String hKey);
+
+	/**
+	 * 获取并移除 队首
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public abstract String lPop(String key);
+
+	/**
+	 * 
+	 * 将 key 的值设为 value ，当且仅当 key 不存在。 若给定的 key 已经存在，则 SETNX 不做任何动作。 SETNX 是『SET
+	 * if Not eXists』(如果不存在，则 SET)的简写。
+	 * 
+	 * @param key
+	 * @param value
+	 * @return 设置成功，返回 1 。设置失败，返回 0 。
+	 */
+	public Boolean setNX(String key, String value);
+
+	/**
+	 * 同{@link #setNX(String, String)}
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public Boolean setNX(byte[] key, byte[] value);
+
+	/**
+	 * 
+	 * <blockquote>
+	 * 
+	 * <pre>
+	 * KEYS pattern
+	 * 
+	 * 查找所有符合给定模式 pattern 的 key 。
+	 * 
+	 * KEYS * 匹配数据库中所有 key 。
+	 * KEYS h?llo 匹配 hello ， hallo 和 hxllo 等。
+	 * KEYS h*llo 匹配 hllo 和 heeeeello 等。
+	 * KEYS h[ae]llo 匹配 hello 和 hallo ，但不匹配 hillo 。
+	 * 特殊符号用 \ 隔开
+	 * 
+	 * KEYS 的速度非常快，但在一个大的数据库中使用它仍然可能造成性能问题，如果你需要从一个数据集中查找特定的 key ，你最好还是用 Redis 的集合结构(set)来代替。
+	 * </pre>
+	 * 
+	 * </blockquote>
+	 * 
+	 * @param patternKey
+	 * @return HashSet
+	 */
+	public Set<String> keys(String patternKey);
+
+	/**
+	 * 
+	 * <blockquote>
+	 * 
+	 * <pre>
+	 * 发布消息
+	 * </pre>
+	 * 
+	 * </blockquote>
+	 * 
+	 * @param msgKey
+	 * @param msg
+	 * @return
+	 */
+	public Long publish(String msgKey, String msg);
+
+	/**
+	 * 乐观锁更新redis,继承有效期;
+	 * <blockquote>
+	 * </blockquote>
+	 * @param key
+	 * @param newValue
+	 * @param oldValue
+	 * @return 有且仅有更新成功返回true
+	 */
+	boolean setByLock(String key, String newValue, String oldValue);
 
 }
--- src/main/java/com/inkyi/redis/service/impl/RedisServiceImpl.java
+++ src/main/java/com/inkyi/redis/service/impl/RedisServiceImpl.java
@@ -1,154 +1,381 @@
 package com.inkyi.redis.service.impl;
 
-import java.io.UnsupportedEncodingException;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.annotation.Resource;
 
-import org.springframework.dao.DataAccessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.ReturnType;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 import com.inkyi.redis.service.RedisService;
 
 public class RedisServiceImpl implements RedisService {
 
-	 private static String redisCode = "utf-8";
+	private static final Logger logger = LoggerFactory
+			.getLogger(RedisServiceImpl.class);
 
-	 @Resource
-	 private RedisTemplate<String, String> redisTemplate;
-	 
-	 
-	    /**
-	     * @param key
-	     */
-	    public long del(final String... keys) {
-	        return (long) redisTemplate.execute(new RedisCallback() {
-	            public Long doInRedis(RedisConnection connection) throws DataAccessException {
-	                long result = 0;
-	                for (int i = 0; i < keys.length; i++) {
-	                    result = connection.del(keys[i].getBytes());
-	                }
-	                return result;
-	            }
-	        });
-	    }
+	@Resource
+	private RedisTemplate<Serializable, Serializable> redisTemplate;
 
-	    /**
-	     * @param key
-	     * @param value
-	     * @param liveTime
-	     */
-	    public void set(final byte[] key, final byte[] value, final long liveTime) {
-	        redisTemplate.execute(new RedisCallback() {
-	            public Long doInRedis(RedisConnection connection) throws DataAccessException {
-	                connection.set(key, value);
-	                if (liveTime > 0) {
-	                    connection.expire(key, liveTime);
-	                }
-	                return 1L;
-	            }
-	        });
-	    }
+	private byte[] getStringSerialize(String value) {
+		return redisTemplate.getStringSerializer().serialize(value);
+	}
 
-	    /**
-	     * @param key
-	     * @param value
-	     * @param liveTime
-	     */
-	    public void set(String key, String value, long liveTime) {
-	        this.set(key.getBytes(), value.getBytes(), liveTime);
-	    }
+	private String getStringDeserialize(byte[] value) {
+		return redisTemplate.getStringSerializer().deserialize(value);
+	}
 
-	    /**
-	     * @param key
-	     * @param value
-	     */
-	    public void set(String key, String value) {
-	        this.set(key, value, 0L);
-	    }
+	private byte[][] getByteParams(String... params) {
+		byte[][] p = new byte[params.length][];
+		for (int i = 0; i < params.length; i++)
+			p[i] = getStringSerialize(params[i]);
+		return p;
+	}
 
-	    /**
-	     * @param key
-	     * @param value
-	     */
-	    public void set(byte[] key, byte[] value) {
-	        this.set(key, value, 0L);
-	    }
+	/**
+	 * 将字符串值 value 关联到 key 。
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	@Override
+	public void set(String key, String value) {
+		set(getStringSerialize(key), getStringSerialize(value));
+	}
 
-	    /**
-	     * @param key
-	     * @return
-	     */
-	    public String get(final String key) {
-	        return (String) redisTemplate.execute(new RedisCallback() {
-	            public String doInRedis(RedisConnection connection) throws DataAccessException {
-	                try {
-	                    return new String(connection.get(key.getBytes()), redisCode);
-	                } catch (UnsupportedEncodingException e) {
-	                    e.printStackTrace();
-	                }
-	                return "";
-	            }
-	        });
-	    }
+	/**
+	 * 将字符串值 value 关联到 key 。
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	@Override
+	public void set(byte[] key, byte[] value) {
+		redisTemplate.execute(new RedisCallback<Object>() {
+			@Override
+			public Object doInRedis(RedisConnection connection) {
+				connection.set(key, value);
+				return null;
+			}
+		});
+	}
 
-	    /**
-	     * @param pattern
-	     * @return 
-	     * @return
-	     */
-	    public Set<String> Setkeys(String pattern) {
-	        return redisTemplate.keys(pattern);
+	@Override
+	public Boolean setNX(String key, String value) {
+		return setNX(getStringSerialize(key), getStringSerialize(value));
+	}
 
-	    }
+	@Override
+	public Boolean setNX(byte[] key, byte[] value) {
+		return redisTemplate.execute(new RedisCallback<Boolean>() {
+			@Override
+			public Boolean doInRedis(RedisConnection connection) {
+				return connection.setNX(key, value);
+			}
+		});
+	}
 
-	    /**
-	     * @param key
-	     * @return
-	     */
-	    public boolean exists(final String key) {
-	        return (boolean) redisTemplate.execute(new RedisCallback() {
-	            public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
-	                return connection.exists(key.getBytes());
-	            }
-	        });
-	    }
+	/**
+	 * 删除
+	 * 
+	 * @param keys
+	 * @return
+	 */
+	@Override
+	public Long del(String... keys) {
+		return del(getByteParams(keys));
+	}
 
-	    /**
-	     * @return
-	     */
-	    public String flushDB() {
-	        return (String) redisTemplate.execute(new RedisCallback() {
-	            public String doInRedis(RedisConnection connection) throws DataAccessException {
-	                connection.flushDb();
-	                return "ok";
-	            }
-	        });
-	    }
+	/**
+	 * 删除
+	 * 
+	 * @param keys
+	 * @return
+	 */
+	@Override
+	public Long del(byte[]... keys) {
+		return redisTemplate.execute(new RedisCallback<Long>() {
+			@Override
+			public Long doInRedis(RedisConnection connection) {
+				return connection.del(keys);
+			}
+		});
+	}
 
-	    /**
-	     * @return
-	     */
-	    public long dbSize() {
-	        return (long) redisTemplate.execute(new RedisCallback() {
-	            public Long doInRedis(RedisConnection connection) throws DataAccessException {
-	                return connection.dbSize();
-	            }
-	        });
-	    }
+	/**
+	 * 获取key
+	 */
+	@Override
+	public String get(String key) {
+		return redisTemplate.execute(new RedisCallback<String>() {
+			@Override
+			public String doInRedis(RedisConnection connection) {
+				return getStringDeserialize(connection
+						.get(getStringSerialize(key)));
+			}
+		});
+	}
 
-	    /**
-	     * @return
-	     */
-	    public String ping() {
-	        return (String) redisTemplate.execute(new RedisCallback() {
-	            public String doInRedis(RedisConnection connection) throws DataAccessException {
+	/**
+	 * 获取key
+	 */
+	@Override
+	public byte[] get(byte[] key) {
+		return redisTemplate.execute(new RedisCallback<byte[]>() {
+			@Override
+			public byte[] doInRedis(RedisConnection connection) {
+				return connection.get(key);
+			}
+		});
+	}
 
-	                return connection.ping();
-	            }
-	        });
-	    }
+	/**
+	 * 执行脚本
+	 * 
+	 * @param script
+	 * @param resultType
+	 * @param value
+	 * @return
+	 */
+	@Override
+	public <T> T eval(String script, Class<T> resultType, String... value) {
+		String scriptSha = scriptLoad(script);
+		return evalSha(scriptSha, resultType, value);
+	}
+
+	/**
+	 * 执行脚本
+	 * 
+	 * @param scriptSha
+	 * @param resultType
+	 * @param value
+	 * @return
+	 */
+	@Override
+	public <T> T evalSha(String scriptSha, Class<T> resultType, String... value) {
+		return redisTemplate.execute(new RedisCallback<T>() {
+			@Override
+			public T doInRedis(RedisConnection connection) {
+				ReturnType returnType = null;
+				if (resultType == null) {
+					returnType = ReturnType.STATUS;
+				} else if (resultType.isAssignableFrom(String.class)) {
+					returnType = ReturnType.STATUS;
+				} else if (resultType.isAssignableFrom(List.class)) {
+					returnType = ReturnType.MULTI;
+				} else if (resultType.isAssignableFrom(Boolean.class)) {
+					returnType = ReturnType.BOOLEAN;
+				} else if (resultType.isAssignableFrom(Long.class)) {
+					returnType = ReturnType.INTEGER;
+				}
+				return connection.evalSha(scriptSha, returnType, value.length,
+						getByteParams(value));
+			}
+		});
+	}
+
+	/**
+	 * list.add
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	@Override
+	public Long lPush(String key, String value) {
+		return redisTemplate.execute(new RedisCallback<Long>() {
+			@Override
+			public Long doInRedis(RedisConnection connection) {
+				return connection.lPush(getStringSerialize(key),
+						getStringSerialize(value));
+			}
+		});
+	}
+
+	@Override
+	public Long lPush(String key, String value, Long seconds) {
+		return redisTemplate.execute(new RedisCallback<Long>() {
+			@Override
+			public Long doInRedis(RedisConnection connection) {
+				byte[] keyB = getStringSerialize(key);
+				Long res = connection.lPush(keyB, getStringSerialize(value));
+				if (seconds != null && seconds > 0)
+					connection.expire(keyB, seconds);
+				return res;
+			}
+		});
+	}
+
+	/**
+	 * list.addall
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	@Override
+	public Boolean expire(String key, Long seconds) {
+		return redisTemplate.execute(new RedisCallback<Boolean>() {
+			@Override
+			public Boolean doInRedis(RedisConnection connection) {
+				byte[] keyB = getStringSerialize(key);
+				return connection.expire(keyB, seconds);
+			}
+		});
+	}
+
+	/**
+	 * list.addall
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	@Override
+	public Long lPush(String key, String... value) {
+		return redisTemplate.execute(new RedisCallback<Long>() {
+			@Override
+			public Long doInRedis(RedisConnection connection) {
+				return connection.lPush(getStringSerialize(key),
+						getByteParams(value));
+			}
+		});
+	}
+
+	/**
+	 * list.size
+	 * 
+	 * @param key
+	 * @return
+	 */
+	@Override
+	public Long lLen(String key) {
+		return redisTemplate.execute(new RedisCallback<Long>() {
+			@Override
+			public Long doInRedis(RedisConnection connection) {
+				return connection.lLen(getStringSerialize(key));
+			}
+		});
+	}
+
+	/**
+	 * list.size
+	 * 
+	 * @param key
+	 * @return
+	 */
+	@Override
+	public Boolean hExists(String key, String value) {
+		return redisTemplate.execute(new RedisCallback<Boolean>() {
+			@Override
+			public Boolean doInRedis(RedisConnection connection) {
+				return connection.hExists(getStringSerialize(key),
+						getStringSerialize(value));
+			}
+		});
+	}
+
+	/**
+	 * 删除目标redis全部DB缓存
+	 */
+	@Override
+	public void flushAll() {
+		redisTemplate.execute(new RedisCallback<Object>() {
+			public Object doInRedis(RedisConnection connection) {
+				connection.flushAll();
+				return null;
+			}
+		});
+	}
+
+	/**
+	 * 预加载脚本
+	 * 
+	 * @param script
+	 * @return sha值
+	 */
+	@Override
+	public String scriptLoad(String script) {
+		return redisTemplate.execute(new RedisCallback<String>() {
+			public String doInRedis(RedisConnection connection) {
+				return connection.scriptLoad(getStringSerialize(script));
+			}
+		});
+	}
+
+	@Override
+	public String lPop(String key) {
+		return redisTemplate.execute(new RedisCallback<String>() {
+			@Override
+			public String doInRedis(RedisConnection connection) {
+				return getStringDeserialize(connection
+						.lPop(getStringSerialize(key)));
+			}
+		});
+	}
+
+	@Override
+	public Set<String> keys(String patternKey) {
+		return redisTemplate.execute(new RedisCallback<Set<String>>() {
+			@Override
+			public Set<String> doInRedis(RedisConnection connection) {
+				Set<byte[]> setC = connection
+						.keys(getStringSerialize(patternKey));
+				Set<String> setT = new HashSet<String>();
+				if (setC != null) {
+					for (byte[] bs : setC) {
+						setT.add(getStringDeserialize(bs));
+					}
+				}
+				return setT;
+			}
+		});
+	}
+
+
+	@Override
+	public Long publish(String msgKey, String msg) {
+		return redisTemplate.execute(new RedisCallback<Long>() {
+			@Override
+			public Long doInRedis(RedisConnection connection) {
+				return connection.publish(getStringSerialize(msgKey),
+						getStringSerialize(msg));
+			}
+		});
+	}
+	
+	private static final String SetByLockScript = ""
+			+ "local oldValue = redis.call('GET', KEYS[1]);\n"
+			+ "if oldValue and type(oldValue)=='string' and oldValue==KEYS[3] then\n"
+			+ "local ttls = redis.call('TTL', KEYS[1]); \n"
+			+ "redis.call('SET',KEYS[1], KEYS[2]);\n"
+			+ "if ttls and ttls>0 then\n"
+			+ "redis.call('EXPIRE',KEYS[1],ttls);\n"
+			+ "end\n"
+			+ "return tostring(true);\n"
+			+ "end\n"
+			+ "return tostring(false)";
+	private static final DefaultRedisScript<String> setByLockScript = new DefaultRedisScript<String>(SetByLockScript, String.class);
+	
+	@Override
+	public boolean setByLock(String key, String newValue, String oldValue) {
+		try {
+			String res = eval(setByLockScript.getScriptAsString(), String.class , key,newValue,oldValue);
+			if(Boolean.TRUE.toString().equals(res)){
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 	   
 }
