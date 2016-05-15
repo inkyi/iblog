5c5b22cdd68b06583f1d0b380baf6e18fcc52b8b
InkYi <lhkmaple@gmail.com>
2016-05-16 02:38:34 +0800

    更新Freemarker为最新版本，增加Redis的Service

---
+++ src/main/java/com/inkyi/redis/service/impl/RedisServiceImpl.java
@@ -0,0 +1,154 @@
+package com.inkyi.redis.service.impl;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Set;
+
+import javax.annotation.Resource;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.inkyi.redis.service.RedisService;
+
+public class RedisServiceImpl implements RedisService {
+
+	 private static String redisCode = "utf-8";
+
+	 @Resource
+	 private RedisTemplate<String, String> redisTemplate;
+	 
+	 
+	    /**
+	     * @param key
+	     */
+	    public long del(final String... keys) {
+	        return (long) redisTemplate.execute(new RedisCallback() {
+	            public Long doInRedis(RedisConnection connection) throws DataAccessException {
+	                long result = 0;
+	                for (int i = 0; i < keys.length; i++) {
+	                    result = connection.del(keys[i].getBytes());
+	                }
+	                return result;
+	            }
+	        });
+	    }
+
+	    /**
+	     * @param key
+	     * @param value
+	     * @param liveTime
+	     */
+	    public void set(final byte[] key, final byte[] value, final long liveTime) {
+	        redisTemplate.execute(new RedisCallback() {
+	            public Long doInRedis(RedisConnection connection) throws DataAccessException {
+	                connection.set(key, value);
+	                if (liveTime > 0) {
+	                    connection.expire(key, liveTime);
+	                }
+	                return 1L;
+	            }
+	        });
+	    }
+
+	    /**
+	     * @param key
+	     * @param value
+	     * @param liveTime
+	     */
+	    public void set(String key, String value, long liveTime) {
+	        this.set(key.getBytes(), value.getBytes(), liveTime);
+	    }
+
+	    /**
+	     * @param key
+	     * @param value
+	     */
+	    public void set(String key, String value) {
+	        this.set(key, value, 0L);
+	    }
+
+	    /**
+	     * @param key
+	     * @param value
+	     */
+	    public void set(byte[] key, byte[] value) {
+	        this.set(key, value, 0L);
+	    }
+
+	    /**
+	     * @param key
+	     * @return
+	     */
+	    public String get(final String key) {
+	        return (String) redisTemplate.execute(new RedisCallback() {
+	            public String doInRedis(RedisConnection connection) throws DataAccessException {
+	                try {
+	                    return new String(connection.get(key.getBytes()), redisCode);
+	                } catch (UnsupportedEncodingException e) {
+	                    e.printStackTrace();
+	                }
+	                return "";
+	            }
+	        });
+	    }
+
+	    /**
+	     * @param pattern
+	     * @return 
+	     * @return
+	     */
+	    public Set<String> Setkeys(String pattern) {
+	        return redisTemplate.keys(pattern);
+
+	    }
+
+	    /**
+	     * @param key
+	     * @return
+	     */
+	    public boolean exists(final String key) {
+	        return (boolean) redisTemplate.execute(new RedisCallback() {
+	            public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+	                return connection.exists(key.getBytes());
+	            }
+	        });
+	    }
+
+	    /**
+	     * @return
+	     */
+	    public String flushDB() {
+	        return (String) redisTemplate.execute(new RedisCallback() {
+	            public String doInRedis(RedisConnection connection) throws DataAccessException {
+	                connection.flushDb();
+	                return "ok";
+	            }
+	        });
+	    }
+
+	    /**
+	     * @return
+	     */
+	    public long dbSize() {
+	        return (long) redisTemplate.execute(new RedisCallback() {
+	            public Long doInRedis(RedisConnection connection) throws DataAccessException {
+	                return connection.dbSize();
+	            }
+	        });
+	    }
+
+	    /**
+	     * @return
+	     */
+	    public String ping() {
+	        return (String) redisTemplate.execute(new RedisCallback() {
+	            public String doInRedis(RedisConnection connection) throws DataAccessException {
+
+	                return connection.ping();
+	            }
+	        });
+	    }
+	   
+}
